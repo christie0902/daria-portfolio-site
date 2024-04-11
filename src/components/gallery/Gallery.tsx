c2e2dbb5d7f4582786d303889c0316b481a10483
Christie Pham <115462474+christie0902@users.noreply.github.com>
2024-04-11 22:23:08 +0200

    add gallery animation

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import "./gallery.scss";
+import { gallery, GalleryObj } from "../../store/gallery-data.js";
+import { useState } from "react";
 
 const Gallery = () => {
-  return (
-    <div>Gallery</div>
-  )
-}
+  const [hoverIndex, setHoverIndex] = useState(-1);
 
-export default Gallery
+  return (
+    <div className="gallery">
+      {gallery.map((image: GalleryObj, index) => {
+        return (
+          <div className="img-container" key={`${image.title} ${index}`}>
+        
+              <div className={`text-container ${hoverIndex === index ? "text-container-active" : ""}`}>
+                <h3>{image.title}</h3>
+                <p>{image.des}</p>
+              </div>
+      
+            <img
+              src={image.img}
+              alt={image.des}
+              onMouseEnter={() => setHoverIndex(index)} 
+              onMouseLeave={() => setHoverIndex(-1)}
+              style={hoverIndex > -1 && hoverIndex !== index ? { filter: "grayscale(80%)" } : {}}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Gallery;
---
+++ src/store/gallery-data.ts
@@ -0,0 +1,79 @@
+export type GalleryObj = { id: number, title: string, des: string, img:string};
+
+export type Gallery = GalleryObj[]
+
+
+export const gallery : Gallery = [
+    {
+        id: 1,
+        title: "Acrylic 1",
+        des: "This is acrylic art",
+        img: '/acrylic.jpg'
+    },
+    {
+        id: 2,
+        title: "Acrylic 2",
+        des: "This is acrylic art",
+        img: '/acrylic1.jpg'
+    },
+    {
+        id: 3,
+        title: "Acrylic 3",
+        des: "This is acrylic art",
+        img: '/acrylic2.jpg'
+    },
+    {
+        id: 4,
+        title: "Acrylic 4",
+        des: "This is acrylic art",
+        img: '/acrylic3.jpg'
+    },
+    {
+        id: 5,
+        title: "Acrylic 5",
+        des: "This is acrylic art",
+        img: '/acrylic4.jpg'
+    },
+    {
+        id: 6,
+        title: "Oil",
+        des: "This is oil art",
+        img: '/oil.jpg'
+    },
+    {
+        id: 7,
+        title: "Watercolor",
+        des: "This is watercolor art",
+        img: '/watercolor.jpg'
+    },
+    {
+        id: 8,
+        title: "Watercolor",
+        des: "This is watercolor art",
+        img: '/watercolor1.jpg'
+    },
+    {
+        id: 9,
+        title: "Watercolor",
+        des: "This is watercolor art",
+        img: '/watercolor2.jpg'
+    },
+    {
+        id: 10,
+        title: "Watercolor",
+        des: "This is watercolor art",
+        img: '/watercolor3.jpg'
+    },
+    {
+        id: 11,
+        title: "Watercolor",
+        des: "This is watercolor art",
+        img: '/wave.jpg'
+    },
+    {
+        id: 10,
+        title: "Watercolor",
+        des: "This is watercolor art",
+        img: '/watercolor.jpg'
+    },
+]
