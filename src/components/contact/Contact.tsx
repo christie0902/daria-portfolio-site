c008d6f0b47a33df59a59c64049674baf0c431ca
Christie Pham <115462474+christie0902@users.noreply.github.com>
2024-04-29 21:45:13 +0200

    add file attachment and preview logic

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./contact.scss";
 
 const Contact = () => {
+  const [attachmentPreviews, setAttachmentPreviews] = useState([]);
+
+  const handleAttachmentChange = (event) => {
+    const files = event.target.files;
+    const previews = [];
+
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        previews.push(reader.result);
+        if (previews.length === files.length) {
+          setAttachmentPreviews(previews);
+        }
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <div className="contact-section">
       <div className="contact-left-section">
@@ -23,15 +44,25 @@ const Contact = () => {
           </div>
           <div className="contact-textfield">
             <label htmlFor="message">Your message</label> <br />
-            <textarea name="message" id="message" cols={60} rows={5}/>
+            <textarea name="message" id="message" cols={60} rows={5} />
           </div>
           <div className="contact-attachment">
             <label htmlFor="attachment">Attach image: </label>
-            <input type="file" name="attachment" id="attachment" />
+            <input
+              type="file"
+              name="attachment"
+              id="attachment"
+              multiple
+              onChange={handleAttachmentChange}
+            />
+          </div>
+          <div className="attachment-preview">
+            {attachmentPreviews?.map((preview, index) => (
+              <img key={index} src={preview} alt={`Attachment ${index + 1}`} />
+            ))}
           </div>
           <button className="contact-button">Submit</button>
         </div>
-        
       </div>
       <div className="contact-photo">
         <img src="acrylic4.jpg" alt="contact" />
@@ -39,7 +70,6 @@ const Contact = () => {
           <p>Email: daria@levitsky.info</p>
         </div>
       </div>
-      
     </div>
   );
 };
