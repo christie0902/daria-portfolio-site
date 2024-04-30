d3618f2913bd8e402f73414b68622dfb25ea0b40
Christie Pham <115462474+christie0902@users.noreply.github.com>
2024-04-30 15:59:47 +0200

    add animation with lazyloader and add responsiveness to contact section

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,95 +1,106 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import LazyLoader from "../../lib/utilityComponents/LazyLoader";
 import "./about.scss";
 
-const About = () => {
-  const elemRef = useRef(null);
-  const [reference, setReference] = useState(null);
+interface AboutProps {
+  reference: HTMLDivElement | null;
+}
 
-  useEffect(() => {
-    setReference(elemRef.current);
-  }, [elemRef]);
+const About: React.FC<AboutProps> = ({reference}) => {
+
 
   return (
-    <div ref={elemRef} className="about-container">
-      <div className="left-container">
-        <div className="title-container">
-          <div className="about-title">
-            ABOUT
-            <br />
-            ME
-          </div>
-          <div className="text-container">
-            <p className="text-uppercase">
-              Welcome to my creative corner! I'm a freelance artist passionate
-              about bringing imagination to life through art.
-            </p>
-            <p className="text-lowercase">
-              With a love for colors, textures, and storytelling, I specialize
-              in [mention your specialties, e.g., acrylic paintings, digital
-              illustrations, etc.]. Join me on this artistic journey as we
-              explore the beauty of expression together.
-            </p>
-          </div>
-        </div>
+    <>
+      {reference && (
+        <LazyLoader targetElement={reference}>
+          <div className="about-container">
+            <div className="left-container">
+              <div className="title-container">
+                <div className="about-title">
+                  ABOUT
+                  <br />
+                  ME
+                </div>
+                <div className="text-container">
+                  <p className="text-uppercase">
+                    Welcome to my creative corner! I'm a freelance artist
+                    passionate about bringing imagination to life through art.
+                  </p>
+                  <p className="text-lowercase">
+                    With a love for colors, textures, and storytelling, I
+                    specialize in [mention your specialties, e.g., acrylic
+                    paintings, digital illustrations, etc.]. Join me on this
+                    artistic journey as we explore the beauty of expression
+                    together.
+                  </p>
+                </div>
+              </div>
 
-        <div className="bottom-container">
-          <h2>MY SERVICES</h2>
-          <div className="services-container">
-            <div className="service-container">
-              <p className="service-number">01</p>
-              <img src="\painting-icon-black.png" alt="painting-icon" />
-              <p className="service-title">Sketch & Painting</p>
+              <div className="bottom-container">
+                <h2>MY SERVICES</h2>
+                <div className="services-container">
+                  <div className="service-container">
+                    <p className="service-number">01</p>
+                    <img src="\painting-icon-black.png" alt="painting-icon" />
+                    <p className="service-title">Sketch & Painting</p>
+                  </div>
+                  <div className="service-container">
+                    <p className="service-number">02</p>
+                    <img src="\digital-icon-black.png" alt="painting-icon" />
+                    <p className="service-title">Digital Art</p>
+                  </div>
+                  <div className="service-container">
+                    <p className="service-number">03</p>
+                    <img src="\camera-icon-black.png" alt="painting-icon" />
+                    <p className="service-title">Photography</p>
+                  </div>
+                  <div className="service-container">
+                    <p className="service-number">04</p>
+                    <img src="\sculpture-icon-black.png" alt="painting-icon" />
+                    <p className="service-title">Sculpture</p>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="service-container">
-              <p className="service-number">02</p>
-              <img src="\digital-icon-black.png" alt="painting-icon" />
-              <p className="service-title">Digital Art</p>
-            </div>
-            <div className="service-container">
-              <p className="service-number">03</p>
-              <img src="\camera-icon-black.png" alt="painting-icon" />
-              <p className="service-title">Photography</p>
-            </div>
-            <div className="service-container">
-              <p className="service-number">04</p>
-              <img src="\sculpture-icon-black.png" alt="painting-icon" />
-              <p className="service-title">Sculpture</p>
+            {/* Art display container */}
+            <div className="art-display">
+              <div>
+                <div className="art-container sculpture">
+                  <p className="art-text sculpture">Sculpture</p>
+                  <img className="art-img" src="acrylic3.jpg" alt="sculpture" />
+                </div>
+                <div className="art-container digital">
+                  <p className="art-text digital">Digital Art</p>
+                  <img
+                    className="art-img"
+                    src="acrylic2.jpg"
+                    alt="digital art"
+                  />
+                </div>
+                <div className="art-container photography">
+                  <p className="art-text photography">Photography</p>
+                  <img
+                    className="art-img"
+                    src="acrylic1.jpg"
+                    alt="photography"
+                  />
+                </div>
+                <div className="art-container sketching">
+                  <p className="art-text sketching">Sketch & Painting</p>
+                  <img
+                    className="art-img"
+                    src="acrylic4.jpg"
+                    alt="sketch and painting"
+                  />
+                </div>
+              </div>
+
+              <a href="#Contact"><button className="contact-button">Contact Me!</button></a>
             </div>
           </div>
-        </div>
-      </div>
-      {/* Art display container */}
-      <div className="art-display">
-        {reference && (
-          <LazyLoader targetElement={reference}>
-            <div>
-              <div className="art-container sculpture">
-                <p className="art-text sculpture">Sculpture</p>
-                <img className="art-img" src="acrylic3.jpg" alt="sculpture" />
-              </div>
-              <div className="art-container digital">
-                <p className="art-text digital">Digital Art</p>
-                <img className="art-img" src="acrylic2.jpg" alt="digital art" />
-              </div>
-              <div className="art-container photography">
-                <p className="art-text photography">Photography</p>
-                <img className="art-img" src="acrylic1.jpg" alt="photography" />
-              </div>
-              <div className="art-container sketching">
-                <p className="art-text sketching">Sketch & Painting</p>
-                <img
-                  className="art-img"
-                  src="acrylic4.jpg"
-                  alt="sketch and painting"
-                />
-              </div>
-            </div>
-          </LazyLoader>
-        )}
-        <button className="contact-button">Contact Me!</button>
-      </div>
-    </div>
+        </LazyLoader>
+      )}
+      </>
   );
 };
 
--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,8 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./contact.scss";
+import LazyLoader from "../../lib/utilityComponents/LazyLoader";
 
 const Contact = () => {
   const [attachmentPreviews, setAttachmentPreviews] = useState([]);
+  const elemRef = useRef(null);
+  const [reference, setReference] = useState(null);
+
+  useEffect(() => {
+    setReference(elemRef.current);
+  }, [elemRef]);
 
   const handleAttachmentChange = (event) => {
     const files = event.target.files;
@@ -24,52 +31,62 @@ const Contact = () => {
   };
 
   return (
-    <div className="contact-section">
-      <div className="contact-left-section">
-        <div className="contact-title">
-          <h3>Daria Alexander</h3>
-          <h1>
-            WORK <br />
-            WITH ME
-          </h1>
-        </div>
-        <div className="contact-form">
-          <div className="contact-field">
-            <label htmlFor="name">Your name</label>
-            <input type="text" name="name" id="name" />
+    <div ref={elemRef}>
+      {reference && (
+        <LazyLoader targetElement={reference}>
+          <div className="contact-section">
+            <div className="contact-left-section">
+              <div className="contact-title">
+                <h3>Daria Alexander</h3>
+                <h1>
+                  WORK <br />
+                  WITH ME
+                </h1>
+              </div>
+              <form className="contact-form">
+                <div className="contact-field">
+                  <label htmlFor="name">Your name</label>
+                  <input type="text" name="name" id="name" />
+                </div>
+                <div className="contact-field">
+                  <label htmlFor="email">Email</label>
+                  <input type="email" name="email" id="email" />
+                </div>
+                <div className="contact-textfield">
+                  <label htmlFor="message">Your message</label> <br />
+                  <textarea name="message" id="message"/>
+                </div>
+                <div className="contact-attachment">
+                  <label htmlFor="attachment">Attach image: </label>
+                  <input
+                    type="file"
+                    name="attachment"
+                    id="attachment"
+                    multiple
+                    onChange={handleAttachmentChange}
+                  />
+                </div>
+                <div className="attachment-preview">
+                  {attachmentPreviews?.map((preview, index) => (
+                    <img
+                      key={index}
+                      src={preview}
+                      alt={`Attachment ${index + 1}`}
+                    />
+                  ))}
+                </div>
+                <button className="contact-button">Submit</button>
+              </form>
+            </div>
+            <div className="contact-photo">
+              <img src="acrylic4.jpg" alt="contact" />
+              <div className="contact-info">
+                <p>Email: daria@levitsky.info</p>
+              </div>
+            </div>
           </div>
-          <div className="contact-field">
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" />
-          </div>
-          <div className="contact-textfield">
-            <label htmlFor="message">Your message</label> <br />
-            <textarea name="message" id="message" cols={60} rows={5} />
-          </div>
-          <div className="contact-attachment">
-            <label htmlFor="attachment">Attach image: </label>
-            <input
-              type="file"
-              name="attachment"
-              id="attachment"
-              multiple
-              onChange={handleAttachmentChange}
-            />
-          </div>
-          <div className="attachment-preview">
-            {attachmentPreviews?.map((preview, index) => (
-              <img key={index} src={preview} alt={`Attachment ${index + 1}`} />
-            ))}
-          </div>
-          <button className="contact-button">Submit</button>
-        </div>
-      </div>
-      <div className="contact-photo">
-        <img src="acrylic4.jpg" alt="contact" />
-        <div className="contact-info">
-          <p>Email: daria@levitsky.info</p>
-        </div>
-      </div>
+        </LazyLoader>
+      )}
     </div>
   );
 };
