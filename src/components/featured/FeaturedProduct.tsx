ff73cd534314dbc4c3dbea5980ec2b0bed53043c
Christie Pham <115462474+christie0902@users.noreply.github.com>
2024-05-02 11:46:09 +0200

    add lazy loader to featured section

--- src/components/featured/FeaturedProduct.tsx
+++ src/components/featured/FeaturedProduct.tsx
@@ -1,22 +1,35 @@
-import React, { useState } from 'react'
-import Book from './Book'
-import "./featured.scss"
+import React, { useEffect, useRef, useState } from "react";
+import Book from "./Book";
+import LazyLoader from "../../lib/utilityComponents/LazyLoader";
+import "./featured.scss";
 
 const FeaturedProduct = () => {
+  const [attachmentPreviews, setAttachmentPreviews] = useState([]);
+  const elemRef = useRef(null);
+  const [reference, setReference] = useState(null);
 
+  useEffect(() => {
+    setReference(elemRef.current);
+  }, [elemRef]);
 
   return (
-    <div className='feature-section'>
-    <h1 className='section-title'>FEATURED ART</h1>
-    <div className="guide">
-      <p className='guide-text'>Flip to explore!</p>
-      <img className="guide-img" src="arrow.png" alt="arrow" />
-    </div>
-    <div className='feature-container'>
-       <Book/>
-    </div>
-    </div>
-  )
-}
 
-export default FeaturedProduct
+          <div className="feature-section" ref={elemRef}>
+          {reference && (
+        <LazyLoader targetElement={reference}>
+            <h1 className="section-title">FEATURED ART</h1>
+            <div className="guide">
+              <p className="guide-text">Flip to explore!</p>
+              <img className="guide-img" src="arrow.png" alt="arrow" />
+            </div>
+            <div className="feature-container">
+              <Book />
+            </div>
+            </LazyLoader>
+                  )}
+          </div>
+ 
+  );
+};
+
+export default FeaturedProduct;
