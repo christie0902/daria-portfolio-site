c9125229a3c2b151493ccdd873c7ea915691d358
Paul Levitsky <85572670+Pasha127@users.noreply.github.com>
2024-05-04 13:29:47 +0200

    Restructure project for fullstack

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,22 @@
-import app from './app';
+import express from 'express';
+import path from 'path';
+import dotenv from "dotenv";
 
-const PORT = process.env.PORT || 5000;
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../views')));
+
+// Routes
+app.get('/api', (req, res) => {
+    res.send('Hello from Express API!');
+});
+
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
