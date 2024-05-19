44743862e6bbf68c261b89c96e77c1d05f972bba
Christie Pham <115462474+christie0902@users.noreply.github.com>
2024-05-19 17:34:12 +0200

    created APIs for load all arts and featured arts

---
+++ backend/src/controllers/api/dataControllers.ts
@@ -0,0 +1,26 @@
+import Art from "../../models/art";
+
+const index = async (req: any, res: any) => {
+    try {
+        const result = await Art.find().sort({ createdAt: -1});
+        res.json(result);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Internal Server Error")
+    }
+}
+
+const featuredArt = async (req: any, res: any) => {
+    try {
+        const result = await Art.find({featured: true}).sort({ createdAt: -1});
+        res.json(result);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Internal Server Error")
+    }
+}
+
+export default {
+    index,
+    featuredArt
+};
