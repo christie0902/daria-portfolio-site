7b47cd49cdb06deb2c6fb85816501d63063297bc
Christie Pham <115462474+christie0902@users.noreply.github.com>
2024-06-13 18:31:08 +0200

    add hamburger menu, complete dark mode for all section

--- frontend/src/components/featured/Page.tsx
+++ frontend/src/components/featured/Page.tsx
@@ -1,5 +1,6 @@
-import React, { forwardRef, ForwardedRef } from 'react'
+import React, { forwardRef, ForwardedRef, useContext } from 'react'
 import { Art } from "../../store/types.ts";
+import ThemeContext from "../../lib/utilityComponents/themeContext";
 
 interface PageProps {
     img: string;
@@ -9,12 +10,14 @@ interface PageProps {
   }
 
   const Page = forwardRef<HTMLDivElement, PageProps>(({ img, title, des, showDetails }, ref) => {
+    const { theme } = useContext(ThemeContext);
+
     return (
       <div className="page-content" ref={ref} >
-        <h1>{title}</h1>
+        <h1 className={`${theme === 'dark' ? 'dark-mode' : ''}`}>{title}</h1>
         <img src={`/uploads/${img}`} alt={title} />
         
-        <p>{des}</p><button onClick={showDetails}>See details</button>
+        <p className={`${theme === 'dark' ? 'dark-mode' : ''}`}>{des}</p><button onClick={showDetails}>See details</button>
       </div>
     );
   });
--- frontend/src/components/navigation/Navigation.tsx
+++ frontend/src/components/navigation/Navigation.tsx
@@ -3,18 +3,27 @@ import "./navigation.scss";
 import NavItem from "./NavItem";
 import ThemeContext from "../../lib/utilityComponents/themeContext";
 
-
 const Navigation = () => {
   const [currentPage, setCurrentPage] = useState("HOME");
   const { theme } = useContext(ThemeContext);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <>
-     <ul className={theme === 'dark' ? 'nav-dark-mode' : ''}>
-        <NavItem currentPage={currentPage} setCurrentPage={setCurrentPage} title="WORK" section="Featured"/>
-        <NavItem currentPage={currentPage} setCurrentPage={setCurrentPage} title="ABOUT" section="About"/>
-        <NavItem currentPage={currentPage} setCurrentPage={setCurrentPage} title="CONTACT" section="Contact"/>
-      </ul>
+      <div className={`nav-container ${theme === 'dark' ? 'nav-dark-mode' : ''}`}>
+        <button className="hamburger-menu" onClick={toggleMenu}>
+          &#9776;
+        </button>
+        <ul className={`nav-items ${isMenuOpen ? 'open' : ''}`}>
+          <NavItem currentPage={currentPage} setCurrentPage={setCurrentPage} title="WORK" section="Featured"/>
+          <NavItem currentPage={currentPage} setCurrentPage={setCurrentPage} title="ABOUT" section="About"/>
+          <NavItem currentPage={currentPage} setCurrentPage={setCurrentPage} title="CONTACT" section="Contact"/>
+        </ul>
+      </div>
     </>
   );
 };
